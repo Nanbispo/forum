12b6f841038ef64656a0c95beb3f073ac7ab02b6
Renan Bispo <contato.renanjesusbispo@gmail.com>
2024-04-26 15:16:57 -0300

    implementação do metodo delete para excluir um topico

--- src/main/kotlin/com/alura/forum/controllers/TopicController.kt
+++ src/main/kotlin/com/alura/forum/controllers/TopicController.kt
@@ -2,11 +2,14 @@ package com.alura.forum.controllers
 
 import com.alura.forum.dtos.TopicForm
 import com.alura.forum.dtos.TopicView
+import com.alura.forum.dtos.toUpdateTopicForm
 import com.alura.forum.services.TopicsServices
 import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -25,7 +28,18 @@ class TopicController(private val service: TopicsServices) {
     }
 
     @PostMapping
-    fun register (@RequestBody @Valid dto: TopicForm){
-        service.register(dto)
+    fun register (@RequestBody @Valid form: TopicForm){
+        service.register(form)
+    }
+
+    @PutMapping
+    fun toUpdate(@RequestBody @Valid form: toUpdateTopicForm){
+        service.toUpdate(form)
+
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long){
+        service.delete(id)
     }
 }
--- src/main/kotlin/com/alura/forum/services/TopicsServices.kt
+++ src/main/kotlin/com/alura/forum/services/TopicsServices.kt
@@ -2,9 +2,11 @@ package com.alura.forum.services
 
 import com.alura.forum.dtos.TopicForm
 import com.alura.forum.dtos.TopicView
+import com.alura.forum.dtos.toUpdateTopicForm
 import com.alura.forum.mappers.TopicFormMapper
 import com.alura.forum.mappers.TopicViewMapper
 import com.alura.forum.models.Topic
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.stereotype.Service
 import java.util.*
 import java.util.stream.Collectors
@@ -33,5 +35,30 @@ class TopicsServices(
         val topic = topicFormMapeer.map(form)
         topic.id = topics.size.toLong() + 1
         topics = topics.plus(topic)
+    }
+
+    fun toUpdate(form: toUpdateTopicForm) {
+        val topic = topics.stream().filter { t ->
+            t.id == form.id
+        }.findFirst().get()
+        topics = topics.minus(topic).plus(
+            Topic(
+                id = form.id,
+                title = form.title,
+                message = form.message,
+                author = topic.author,
+                course = topic.course,
+                creationDate = topic.creationDate,
+                status = topic.status,
+                answers = topic.answers
+            )
+        )
+    }
+
+    fun delete(id: Long) {
+        val topic = topics.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+        topics = topics.minus(topic)
     }
 }
