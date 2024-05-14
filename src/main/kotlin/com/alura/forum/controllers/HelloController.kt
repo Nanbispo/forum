9b5997af628f0d218b9899d340a6c9dde385c924
Renan Bispo <contato.renanjesusbispo@gmail.com>
2024-05-14 16:48:28 -0300

    ...

--- src/main/kotlin/com/alura/forum/controllers/HelloController.kt
+++ src/main/kotlin/com/alura/forum/controllers/HelloController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/helloWorld")
+@RequestMapping("/hello")
 class HelloController {
     @GetMapping
     fun hello(): String{
