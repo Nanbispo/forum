78f7948f1129999fe7e393efd43c0ad05be1a8f7
Renan Bispo <contato.renanjesusbispo@gmail.com>
2024-05-14 22:59:34 -0300

    implement: implementando as roles no usuario

--- src/main/kotlin/com/alura/forum/models/User.kt
+++ src/main/kotlin/com/alura/forum/models/User.kt
@@ -1,5 +1,6 @@
 package com.alura.forum.models
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.*
 
 @Entity
@@ -9,5 +10,10 @@ data class User (
     val id: Long? = null,
     val name: String,
     val email: String,
-    val password: String
+    val password: String,
+
+    @JsonIgnore
+    @JoinTable(name = "users_role")
+    @ManyToMany
+    val role: List<Role> = mutableListOf()
 )
